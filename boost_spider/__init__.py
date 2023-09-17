5347e2bff3ec86eca3eea3fa9635db2db2b17bf5
ydf0509 <909686719@qq.com>
2023-09-17 21:32:05 +0800

    commit

--- boost_spider/__init__.py
+++ boost_spider/__init__.py
@@ -2,6 +2,7 @@
 import json
 import re
 from funboost import *
+from parsel import Selector
 from .request_client import RequestClient
 from .sink.momgo_sink import MongoSink
 from .sink.mysql_sink import MysqlSink

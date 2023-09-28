58b7eb4d1f1cec28990c9ef5a73ed662d5e44e57
LAPTOP-7V78BBO2\ydf19 <yangdf@akulaku.com>
2023-09-28 15:09:26 +0800

    commit

--- boost_spider/sink/json_sink.py
+++ boost_spider/sink/json_sink.py
@@ -6,18 +6,29 @@ from pathlib import Path
 
 class JsonFileSink:
     _lock = threading.Lock()
+    _has_mkdir = False
 
     def __init__(self, path, file):
-        Path(path).mkdir(exist_ok=True)
+        if not self._has_mkdir:
+            self.__class__._has_mkdir = True
+            Path(path).mkdir(exist_ok=True)
         self.full_path = Path(path) / Path(file)
 
     def save(self, item: dict):
         with self._lock:
             with Path(self.full_path).open('a+', encoding='utf8') as f:
                 item['item_insert_time'] = str(datetime.datetime.now())
-                f.write(json.dumps(item) + '\n')
+                f.write(json.dumps(item) + ',\n')
+
+    def read_json(self):
+        text = Path(self.full_path).read_text(encoding='utf8')
+        json_text = f'[{text[:-2]}]'
+        return json.loads(json_text)
 
 
 if __name__ == '__main__':
-    JsonFileSink('/codedir', 'testjsonfile.json').save({'a': 1, 'b': '44'})
-    # JsonFileSink('/codedir', 'testjsonfile.json').save({'a': 63, 'b': 4})
+    for i in range(100):
+        # JsonFileSink('/codedir', 'testjsonfile2.json').save({'a': i, 'b': f'{i*2}'})
+        # JsonFileSink('/codedir', 'testjsonfile.json').save({'a': 63, 'b': 4})
+        pass
+    print(JsonFileSink('/codedir', 'testjsonfile2.json').read_json())
